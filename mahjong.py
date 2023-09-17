66c3c178fe0bb992eaaa0475f84597711e2ed29f
habacat <pb_hbc@163.com>
2023-09-17 13:49:06 +0800

    testjson

--- mahjong.py
+++ mahjong.py
@@ -4,10 +4,6 @@
 from check import CheckWin
 import pandas as pd
 import numpy as np
-from flask import Flask, request, jsonify
-import requests
-
-app = Flask(__name__)
 
 def InitCards(df, player_names, player_flags_write, player_flags, cards): # 定义函数，用来重新生成13张牌组
 	player_flags_write.fill(0) # 四个玩家的数据写入指针初始化
@@ -41,11 +37,8 @@ def RepeatOnce(df, cards, pf, pfw, i, name, player_names, winner_names, winner_s
 			print("发生未知错误...程序终止！")
 			return False, False, False
 
-def solve(filename):
-	with open(filename, 'r', encoding='utf-8') as file: # 读取文件
-		dealer = file.readline().strip().replace("," , "") # 读取文件的第一行作为初始的dealer（摸牌者）
-		# print("initial_dealer=",dealer)
-	df = pd.read_csv(filename, encoding='utf-8', skiprows=1) # type(data) --> <class 'pandas.core.frame.DataFrame'>
+def solve(data_json):
+	df = pd.read_json(data_json, encoding='utf-8', skiprows=1) # type(data) --> <class 'pandas.core.frame.DataFrame'>
 
 	p1_name, p2_name, p3_name, p4_name = df.columns # 获得4个玩家的名字
 	pn = player_names = np.array([p1_name, p2_name, p3_name, p4_name]) # 将四个名字存储在player_names中，方便取用
@@ -112,51 +105,3 @@ def solve(filename):
 				else: # 如果玩家指针越界，则抛出警告，中止程序，返回玩家指针位置
 					print("数组坐标越界，本局游戏中止。终止位置：",pf)
 					return pn, winner_names, winner_scores
-
-
-def hello_world2():
-    return '仅支持post请求...'
-
-
-# @app.route('/<path:path>', methods=['GET', 'POST'])
-# def main():
-# 	filename = "test1.csv"
-# 	try:
-# 		filename = f"{path}"
-# 		if(filename is None):
-# 			raise IndexError
-# 	except IndexError:
-# 		print("缺少输入参数，请正确引用文件名 *.csv\n示例：/test1.csv\n")
-# 		print("本次将使用main函数内的filename默认值启动...")
-# 		pass
-# 	except:
-# 		print("发生未知错误...程序终止！"); exit()
-# 	try:
-# 		player_names, winner_names, winner_scores = solve(filename)
-# 		if(player_names or winner_names or winner_scores == False):
-# 			print("返回值错误，程序退出！")
-# 		else:
-# 			print("正在为您生成winner.csv文件...")
-# 	except:
-# 		print("solve(filename)无法返回预期的值...程序强制退出...")
-# 		exit()
-# 	try:
-# 		if os.path.exists("winner.csv"):
-# 			os.rename("winner.csv", "winner.bak.csv")
-# 			print("原目录中已存在winner.csv文件，已经将原文件重命名为winner.bak.csv，请及时保存。")
-# 		if(np.all(winner_scores) == 0): # 如果没有人获胜（winner_scores全是0），则输出Draw
-# 			with open('winner.csv', 'w', encoding='utf-8') as file:
-# 				file.writelines("Draw\n")
-# 				for name, score in zip(player_names, winner_scores): # 遍历player_names数组和winner_scores列表
-# 					file.write(f'{name},{score*100:.2f}%\n') # 将每个元素的名称和得分以指定格式写入文件
-# 			print("无人获胜（Draw）时的winner.csv成功生成。")
-# 		else:
-# 			winner_scores = winner_scores/np.sum(winner_scores)
-# 			np.savetxt('winner.csv', winner_names, fmt='%s', encoding='utf-8')
-# 			with open('winner.csv', 'a', encoding='utf-8') as file:
-# 				for name, score in zip(player_names, winner_scores): # 遍历player_names数组和winner_scores列表
-# 					file.write(f'{name},{score*100:.2f}%\n') # 将每个元素的名称和得分以指定格式写入文件
-# 			print("有玩家获胜，winner.csv成功生成。")
-# 	except:
-# 		print("保存文件时出错！")
-# 		exit()
--- main.py
+++ main.py
@@ -19,20 +19,38 @@ if __name__ == '__main__':
 def hello_world():
 	return 'Hello from Flask Github!'
 
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['GET','POST'])
 def run_slove():
 	if request.method == 'POST':
 		data = request.form.get('data')  # 获取名为"data"的POST参数
+		winner_names, player_names, winner_scores = solve(data)
+		fin = np.empty((0,))
+		if(not (winner_names and player_names and winner_scores == False)):
+			if(np.all(winner_scores) == 0):
+				fin = np.append(fin, 'Draw')
+				for name, score in zip(player_names, winner_scores): # 遍历player_names数组和winner_scores列表
+					fin = np.append(fin, f'{name},{score*100:.2f}%\n') # 将每个元素的名称和得分以指定格式写入文件
+			else:
+				winner_scores = winner_scores/np.sum(winner_scores)
+				fin = np.append(fin, winner_names)
+				for name, score in zip(player_names, winner_scores): # 遍历player_names数组和winner_scores列表
+					fin = np.append(fin, f'{name},{score*100:.2f}%\n')
+			fin_2d = np.expand_dims(fin, axis=0)
+			transposed_fin = fin_2d.T
+			# 创建一个包含三个列表的字典
+			data = {
+				'winner_names': winner_names.tolist(),
+				'player_names': player_names.tolist(),
+				'winner_scores': winner_scores.tolist(),
+				'finnal_csv': transposed_fin.tolist()
+			}
+			return jsonify(data)
+		else:
+			data = {
+				'data' : '传递的数据有误'
+			}
+			return jsonify(data)
 
-		# 在这里处理接收到的数据
-		# ...
-
-		return "Received POST data: " + data
-	list1,list2,list3 = solve("test1.csv")
-	# 创建一个包含三个列表的字典
-	data = {
-		'winner_names': list1.tolist(),
-		'player_names': list2.tolist(),
-		'player_scores': list3.tolist()
-	}
-	return jsonify(data)
+	if request.method == 'POST':
+		return '请使用POST方法，向 https://python-vercel-test-one.vercel.app/api 传递data(json格式)\n \
+			data需要为使用df.to_json(orient=\'records\')将dataframe转换为json。'
