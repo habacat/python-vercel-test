8184b2e659c4e9bb7825799bd2d64daccc0352be
habacat <pb_hbc@163.com>
2023-09-17 11:59:49 +0800

    index

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(port=5000)
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello from Flask Github!'
